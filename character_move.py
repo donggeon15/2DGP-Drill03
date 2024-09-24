324c8a4213c1100c6d467e892e8b1158ec47d28d
dongPC@notebook <96215716+donggeon15@users.noreply.github.com>
2024-09-24 12:13:32 +0900

    원 운동 구현 완료2

--- character_move.py
+++ character_move.py
@@ -6,22 +6,20 @@ import math
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+boy = load_image('character.png')
 
 def run_circle():
     print('CIRCLE')
 
-    r = 300
-    cx = 400
-    cy = 300
-
+    r, cx, cy = 300, 800/2, 600/2
+    
     for degree in range(0,360,3):
         theta = math.radians(degree)
         x = r * math.cos(theta) + cx
         y = r * math.sin(theta) + cy
 
         clear.canvas_row()
-        character.draw_now(x, y)
+        boy.draw_now(x, y)
         delay(0.1)
 
 
