64ffdf20822594ed59abc414568b963b3ade1da9
dongPC@notebook <96215716+donggeon15@users.noreply.github.com>
2024-09-24 11:49:47 +0900

    자료 추가

---
+++ character_grass.py
@@ -0,0 +1,19 @@
+from pico2d import *
+
+open_canvas()
+
+# fill here
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+
+x = 0
+while (x < 800):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x,90)
+    x = x + 5
+    delay(0.05)
+
+
+close_canvas()
