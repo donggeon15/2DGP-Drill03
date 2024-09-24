c9ece2d74475c89f934e40c456b218453ca4bc9c
dongPC@notebook <96215716+donggeon15@users.noreply.github.com>
2024-09-24 11:50:08 +0900

    뼈대 완성

---
+++ character_movemove.py
@@ -0,0 +1,24 @@
+from pico2d import *
+import math
+
+#open_canvas()
+ 
+
+grass = load_image('grass.png')
+boy = load_image('character.png')
+
+def row_rectangle():
+    print('CIRCLE')
+    pass
+
+def row_circle():
+    print('CIRCLE')
+    pass
+
+# fill here
+
+while (True):
+    row_rectangle()
+    row_circle()
+
+#close_canvas()
